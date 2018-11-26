47f247edac2ed6f60c2a7b915ecd52d05863da7f
Mallory Coleman <mallorycoleman@Mallorys-Air.fc.ca>
2018-11-26 16:05:32 -0500

    index

--- python_files/canadaline.py
+++ python_files/canadaline.py
@@ -6,6 +6,7 @@ years = ('1920', '1930', '1940', '1950', '1960', '1970', '1980', '1990', '2000',
 medals = (21, 33, 20, 37, 48, 4, 12, 126, 143, 180)
 
 plt.scatter(years, medals, c='g')
+plt.title('Canada Medals over the Years')
 plt.xlabel('Years')
 plt.ylabel('Medals')
 plt.show()
--- python_files/usaline.py
+++ python_files/usaline.py
@@ -6,6 +6,7 @@ years = ('1920', '1930', '1940', '1950', '1960', '1970', '1980', '1990', '2000',
 medals = (27, 57, 16, 56, 42, 36, 46, 69, 136, 161)
 
 plt.scatter(years, medals, c='g')
+plt.title('USA Medals over the Years')
 plt.xlabel('Years')
 plt.ylabel('Medals')
 plt.show()
